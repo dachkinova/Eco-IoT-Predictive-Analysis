97e940c8f17f5bbeafb07bbe41b1f03a2fae66ed
Angelina Dachkinova <56481374+dachkinova@users.noreply.github.com>
2024-03-19 18:18:54 +0200

    Read data from multiple sheets

--- read_data.py
+++ read_data.py
@@ -4,29 +4,33 @@ import numpy as np
 from datetime import datetime
 
 os.chdir("D:\\TU\\PEIN\\data")
+sheet_list = ['pavlovo', 'drujba', 'hipodruma']
 dayFile = pandas.read_excel('day.xls',
-                        sheet_name='pavlovo', 
+                        sheet_name=sheet_list, 
                         header=1, 
                         usecols="A:C", 
                         dtype={"Date": 'string', "O3": float, "PM10": float})
 hourFile = pandas.read_excel('hour.xls',
-                        sheet_name='pavlovo', 
+                        sheet_name=sheet_list, 
                         header=1, 
                         usecols={ "Date", "NO", "NO2", "AirTemp","Press", "UMR" }, 
                         dtype={"Date": 'string', "NO": float, "NO2": float, "AirTemp": float, "Press": float, "UMR": float})
 merged_df = pandas.DataFrame()
 
-for i in range(dayFile["Date"].size):
-    date = dayFile.iloc[i]["Date"]
-    O3 = dayFile.iloc[i]["O3"]
-    PM10 = dayFile.iloc[i]["PM10"]
+for sheet_name, daySheet in dayFile.items():
+    for i in range(daySheet["Date"].size):
+        
+        date = daySheet.iloc[i]["Date"]
+        O3 = daySheet.iloc[i]["O3"]
+        PM10 = daySheet.iloc[i]["PM10"]
+        
+        hourSheet = hourFile[sheet_name]
     
-    filtered_hour = hourFile[hourFile["Date"].str.contains(date)]
+        filtered_hour = hourSheet[hourSheet["Date"].str.contains(date)]
+        filtered_hour.insert(2, "O3", O3)
+        filtered_hour.insert(3, "PM10", PM10)
 
-    filtered_hour.insert(2, "O3", O3)
-    filtered_hour.insert(3, "PM10", PM10)
-
-    merged_df = merged_df._append(filtered_hour, ignore_index=True)
+        merged_df = merged_df._append(filtered_hour, ignore_index=True)
 
 merged_df = merged_df.dropna()
 
